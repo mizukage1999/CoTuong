f37f21634d8cbbf098e67a2b98181a9e887a9dbb
MadDog1499 <tien.nguyen.codeff@gmail.com>
2021-04-12 17:12:46 +0700

    Update notify.js
    Tạo push notification hỏi user

--- js/notify.js
+++ js/notify.js
@@ -18,3 +18,35 @@ export function flashTitle(msg) {
     }
     window.addEventListener('focus', clear);
 }
+//Push notification to ask user
+export function pushAsk(callback) {
+    function checkNotificationPromise() {
+        try {
+            Notification.requestPermission().then();
+        } catch {
+            return false;
+        }
+        return true;
+    }
+
+    function handlePermission(permission) {
+        if(!('permission' in Notification)) {
+            Notification.permission = permission;
+        }
+        callback(permission);
+    }
+    if (!"Notification" in window) {
+        console.log("This browser does not support notifications.");
+    } else {
+        if (checkNotificationPromise()) {
+            Notification.requestPermission()
+            .then(function (permission) {
+                handlePermission(permission);
+            })
+        } else {
+            Notification.requestPermission(function(permission) {
+                handlePermission(permission);
+            });
+        }
+    }
+}
