e3635d988cb7f51cc944df6a98f99c2caa3baa26
mizukage1999 <62182994+mizukage1999@users.noreply.github.com>
2021-04-16 00:04:40 +0700

    update
    update

--- js/PeerCom.js
+++ js/PeerCom.js
@@ -18,17 +18,11 @@ export default class PeerCom extends EventTarget {
      * begins a connection with that Peer.  Otherwise, it calls the wait callback 
      * function, passing a connection ID.  The user must send this ID to their 
      * peer so that they can connect.
-     *
-     * Several events may be dispatched while the peers are connecting/connected:
-     * - wait
-     * - connectedslave
-     * - connectedmaster
-     * - disconnected
      * 
      * @param {String} pId If given, the peer ID to connect to.
      */
     begin(pId=null) {
-        console.log('Connecting to Peer server.');
+        // console.log('Connecting to Peer server.');
         this.peerId = pId;
         if (lastId) {
             this._peer = new Peer(lastId);
@@ -36,53 +30,8 @@ export default class PeerCom extends EventTarget {
         else {
             this._peer = new Peer();
         }
-
-        let ondisconnected = function () {
-            console.log("Data connection has been closed.");
-            this.isConnected = false
-            this.dispatchEvent(new Event('disconnected'));
-            this._conn = null;
-            this.peerId = null;
-        }.bind(this);
-
-        let onconnected = function () {
-            console.log('Connected to peer at ID: ' + this._conn.peer);
-            this.dispatchEvent(new CustomEvent('connectedpeer', {
-                detail: this._conn.peer
-            }));
-            this._conn.on('data', this._received.bind(this)); // Call received when we receive data.
-            this.peerId = this._conn.peer;
-            this.isConnected = true;
-        }.bind(this);
-
-        let onconnect = function (conn) {
-            this._conn = conn;
-            this._conn.on('open', onconnected);
-            this._conn.on('close', ondisconnected);
-        }.bind(this);
-
-        let onopen = function (id) {
-            console.log('Established connection to Peer server. My ID: ' + id);
-            if (sessionStorage) {
-                sessionStorage.setItem('myId', id);
-            }
-            if (pId) { // Connect to the peer.
-                console.log('Connecting to peer at ID: ' + pId);
-                onconnect(this._peer.connect(pId));
-            } else { // or wait for a connection
-                console.log('Waiting for connection from peer.')
-                this.dispatchEvent(new CustomEvent('wait', { detail: id }));
-            }
-            this._peer.on('connection', onconnect);
-        }.bind(this);
-
-        this._peer.on('open', onopen);
     }
 
-    disconnect() {
-        if (this._conn) { this._conn.close(); }
-        if (this._peer) { this._peer.disconnect(); }
-    }
 
     /**
      * Received data from a peer.
@@ -121,12 +70,12 @@ export default class PeerCom extends EventTarget {
      * @param {String} type String that describes the data.
      * @param {*} data Data to send.
      */
-    send(type, data) {
-        if (!this._conn) { throw new Error('Connection not established!'); }
-        this._conn.send({
-            'type': type,
-            'data': data
-        });
-    }
+    // send(type, data) {
+    //     if (!this._conn) { throw new Error('Connection not established!'); }
+    //     this._conn.send({
+    //         'type': type,
+    //         'data': data
+    //     });
+    // }
 }
 
--- js/app.js
+++ js/app.js
@@ -3,10 +3,7 @@ import * as storage from './storage.js';
 import PeerCom from './PeerCom.js';
 import Board from './chess/board.js';
 
-/*
-import test from './chess/test.js';
-test();
-*/
+
 
 const elBody = document.getElementById('xiangqi_board_game');
 
@@ -14,36 +11,23 @@ const elBody = document.getElementById('xiangqi_board_game');
 const elGame = document.getElementById('game');
 const elBoard = document.getElementById('board');
 
-// const elBtnChat = document.getElementById('btnChat');
-const elBtnSettings = document.getElementById('btnSettings');
 
+const elBtnSettings = document.getElementById('btnSettings');
 const elSndCapture = document.getElementById('sndCapture');
 const elSndCheck = document.getElementById('sndCheck');
 const elSndMove = document.getElementById('sndMove');
 
 // Forms
-// const elBtnOnline = document.getElementById('btnOnline');
 const elBtnLocal = document.getElementById('btnLocal');
-const elBtnComputer = document.getElementById('btnComputer');
 
 const elBtnCloseGameover = document.getElementById('btnCloseGameover');
-// const elBtnCopyUrl = document.getElementById('btnCopyUrl');
-
-// const elMsgUrl = document.getElementById('msgUrl');
 const elMsgResult = document.getElementById('msgResult');
 
-// const elPeerShow = document.getElementById('peerShow');
-// const elPeerWait = document.getElementById('peerWait');
 
 const elSelFirstMove = document.getElementById('selFirstMove');
 const elSelHandicap = document.getElementById('selHandicap');
 const elSelSkill = document.getElementById('selSkill');
-const elBtnComputerStart = document.getElementById('btnComputerStart');
 
-// Chat
-const elMessages = document.getElementById('bubbles');
-const elTxtMsg = document.getElementById('txtMsg');
-const elBtnSendMsg = document.getElementById('btnSendMsg');
 
 // Controls
 const elBtnUndo = document.getElementById('btnUndo');
@@ -59,7 +43,6 @@ const elChkNotiPush = document.getElementById('chkNotiPush');
 const MODALS = [
     'mod_gameselect',
     'mod_waiting',
-    // 'mod_disconnected',
     'mod_computer',
     'mod_gameover'
 ];
@@ -88,7 +71,7 @@ let main = function () {
     let peerCom = new PeerCom();
 
     if (peerId !== null) {
-        console.log('I am slave');
+        // console.log('Test');
         peerCom.begin(peerId);
         showModal('mod_waiting');
     } else {
@@ -104,11 +87,7 @@ let main = function () {
     let start = function (computer=false, skill=0) {
         board.animated = (storage.getItem('animated') !== 'disabled');
 
-        if (computer) { // vs. computer
-            board.setSearch(16);
-            board.millis = Math.pow(10, skill + 1);
-            board.response();
-        }
+        
 
         let onmove = function(evt) {
             let move = evt.detail;
@@ -186,80 +165,20 @@ let main = function () {
     elSelSkill.addEventListener('change', skillChange);
     elSelSideSkill.addEventListener('change', skillChange);
 
-    // elBtnOnline.addEventListener('click', function () {
-    //     showModal('mod_shareurl');
-    // });
-
     elBtnLocal.addEventListener('click', function () {
         board = new Board(elBoard, TYPE_LOCAL, false, 0);
         start();
         hideModals();
     });
 
-    elBtnComputer.addEventListener('click', function() {
-        showModal('mod_computer');
-    });
-
-    elBtnComputerStart.addEventListener('click', function() {
-        board = new Board(elBoard, 1 - elSelFirstMove.selectedIndex, false, elSelHandicap.selectedIndex);
-        start(true, elSelSkill.selectedIndex);
-        hideModals();
-    });
-
-    // elMsgUrl.addEventListener('click', function () {
-    //     window.getSelection().selectAllChildren(elMsgUrl);
-    // });
-
-    // elBtnCopyUrl.addEventListener('click', function () {
-    //     window.getSelection().selectAllChildren(elMsgUrl);
-    //     document.execCommand('copy');
-    // });
-
-    // elBtnChat.addEventListener('click', function () {
-    //     elBody.classList.toggle('chat');
-    //     if (elBody.classList.contains('chat')) {
-    //         elBody.classList.remove('unread');
-    //     }
-    //     resize();
-    // });
-
-    elBtnSettings.addEventListener('click', function () {
-        elBody.classList.toggle('settings');
-        resize();
-    });
-
-    elBtnCloseGameover.addEventListener('click', function () {
-        hideModals();
-    });
-
-    elBtnSendMsg.addEventListener('click', function() {
-        let msg = elTxtMsg.value;
-        elTxtMsg.value = '';
-        if (msg.replace(/\s/g, '').length) {
-            peerCom.send('Message', msg);
-            let html = '';
-            html += '<div class="sent">';
-            html += '<span class="person">You</span>';
-            html += '<span class="content"><span>' + msg + '</span></span>';
-            html += '</div>';
-            elMessages.innerHTML += html;
-        }
-        elMessages.scrollTop = elMessages.scrollHeight;
-    });
-
-    elTxtMsg.addEventListener('keypress', function (evt) {
-        if (evt.keyCode === 13 && !evt.shiftKey) {
-            evt.preventDefault();
-            elBtnSendMsg.click();
-        }
-    });
+    
 
     elBtnUndo.addEventListener('click', function (evt) {
         if (board) { board.retract(); }
     });
-    elBtnRedo.addEventListener('click', function (evt) {
-        if (board) { board.expand() ; }
-    });
+    // elBtnRedo.addEventListener('click', function (evt) {
+    //     if (board) { board.expand() ; }
+    // });
 
 
     let setBoardSize = function () {
@@ -321,92 +240,12 @@ let main = function () {
         elChkNotiPush.checked = (storage.getItem('notiPush') === 'enabled');
     }
 
-    // Peer2Peer
-    // peerCom.addEventListener('wait', function (evt) {
-    //     let peerUrl = new URL(window.location.href);
-    //     peerUrl.searchParams.set('peerId', evt.detail);
-
-    //     console.log('Have peer connect to: ' + peerUrl);
-    //     elMsgUrl.innerHTML = peerUrl.href;
-
-    //     peerWait.classList.toggle('hide');
-    //     peerShow.classList.toggle('hide');
-    // });
-
-    peerCom.addEventListener('connectedpeer', function (evt) {
-        console.log('A peer connected');
-
-        if (!peerId) { // New game
-            board = new Board(elBoard, TYPE_FIRSTMOVE, true, 0);
-            start();
-            hideModals();
-        }
-        peerId = evt.detail;
-
-        if (board) { // Send start signal to peer
-            peerCom.send('Start', {
-                type: board.computer,
-                moves: board.pos.mvList
-            });
-        }
-
-        // Append URL with peerId
-        let peerUrl = new URL(window.location.href);
-        peerUrl.searchParams.set('peerId', peerId);
-        window.history.pushState({ path: peerUrl.href }, '', peerUrl.href);
-    });
-
-    // peerCom.addEventListener('disconnected', function () {
-    //     console.log('Peer disconnected')
-    //     showModal('mod_disconnected');
-    // });
-
-    // Peer2Peer Data
-    // Games Start / Reconnect
-    peerCom.addReceiveHandler('Start', function (obj) {
-        board = new Board(elBoard, (obj.type) ? 0 : 1, true, 0);
-        for (let i = 1, len = obj.moves.length-1; i < len; ++i) {
-            board.addMove(obj.moves[i], board.computerMove());
-        }
-        start();
-        board.addMove(obj.moves[obj.moves.length-1], board.computerMove());
-        hideModals();
-    });
-
-    // Move
-    let notiMove = null;
-    peerCom.addReceiveHandler('Move', function (move) {
-        board.addMove(move.move, true);
-        if (!document.hasFocus()) {
-            if (storage.getItem('notiPush') === 'enabled') {
-                // We should check for gameover here
-                notiMove = notify.pushNotify('Your Move');
-            }
-        }
-    });
+    
     window.addEventListener('focus', function(evt) {
         if (notiMove) { notiMove.close(); }
     });
 
-    // Chat
-    // peerCom.addReceiveHandler('Message', function(msg) {
-    //     let html = '';
-    //     html += '<div class="received">';
-    //     html += '<span class="person">Opponent</span>';
-    //     html += '<span class="content"><span>' + msg + '</span></span>';
-    //     html += '</div>';
-    //     elMessages.innerHTML += html;
-    //     elMessages.scrollTop = elMessages.scrollHeight;
-    //     if (!document.hasFocus()) {
-    //         if (storage.getItem('notiPush') === 'enabled') {
-    //             let noti = notify.pushNotify('New Message', 'Opponent: ' + msg);
-    //             window.setTimeout(noti.close.bind(noti), 5000);
-    //         }
-    //     }
-    //     if (!elBody.classList.contains('chat')) {
-    //         elBody.classList.add('unread');
-    //     }
-    // });
+    
 }
 
 main();
